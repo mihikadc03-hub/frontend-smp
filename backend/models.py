094d34dab861288fd06d132c28a54900eb8ab672
mihikadc03 <mihikadc03@gmail.com>
2026-06-05 21:10:39 +0530

    Integrated Neon PostgreSQL database and finalized backend API

---
+++ backend/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class ProductModel(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    category = Column(String)
+    price = Column(Float)
+    image = Column(String)
+    description = Column(String)
+
+class CartItemModel(Base):
+    __tablename__ = "cart"
+    id = Column(Integer, primary_key=True, index=True)
+    product_id = Column(Integer)
+    quantity = Column(Integer)
+
+class OrderModel(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    total_price = Column(Float)
+    status = Column(String, default="Pending")
